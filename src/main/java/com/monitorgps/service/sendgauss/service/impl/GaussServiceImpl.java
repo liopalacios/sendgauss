b448d9270e6d5106b9205a89e42ac1962d4b0056
LEYTER <lpalacios@certicom.com.pe>
2020-01-30 16:02:25 -0500

    second cambios

--- src/main/java/com/monitorgps/service/sendgauss/service/impl/GaussServiceImpl.java
+++ src/main/java/com/monitorgps/service/sendgauss/service/impl/GaussServiceImpl.java
@@ -11,6 +11,9 @@ import com.monitorgps.service.sendgauss.service.GaussService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -28,18 +31,25 @@ public class GaussServiceImpl implements GaussService {
     AlertaActivaRepository alertaActivaRepository;
     @Override
     public ListResponseGauss sendEvents(Date horaant, Date horaact) {
-        Pageable pageable = PageRequest.of(0,300);
+        String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlc1Blcm1pc3Npb25zIjpbeyJyb2xlTmFtZSI6IkFwaUluamVjdGlvbiIsInBlcm1pc3Npb25zIjpbXX1dLCJ1c2VyX25hbWUiOiJ0ZXN0aW5nQHJydnNhYy5jb20iLCJsYW5ndWFnZSI6ImVzIiwibGlua2VkUGVvcGxlSWQiOm51bGwsInVzZXJOYW1lIjoidGVzdGluZ0BycnZzYWMuY29tIiwidXNlcklkIjozOCwiYXV0aG9yaXRpZXMiOlsiQXBpSW5qZWN0aW9uIl0sImNsaWVudF9pZCI6ImdhdXNzY29udHJvbGFwaSIsImF1ZCI6WyJ0ZXN0and0cmVzb3VyY2VpZCJdLCJzY29wZSI6WyJyZWFkIiwid3JpdGUiXSwicm9vdCI6ZmFsc2UsIm5hbWUiOiJSUlYiLCJ0ZW5hbnRJZCI6InRyYW5zdmlwIiwibGlua2VkUGVvcGxlQ29kZSI6bnVsbCwiTGlua2VkUGVvcGxlVGFncyI6W10sImV4cCI6MTU4Mjc1MDA1MywidGFnc1Blcm1pc3Npb25zIjpbeyJ0YWciOiIqIiwidGFnQ2F0ZWdvcnkiOiJPUEVSQVRJT05TIn0seyJ0YWciOiIqIiwidGFnQ2F0ZWdvcnkiOiJQTEFOVCJ9LHsidGFnIjoiKiIsInRhZ0NhdGVnb3J5IjoiQ0FSUklFUiJ9XSwianRpIjoiNGVjZTc5NWEtNDAzNy00Y2JiLThhN2UtNThmNDE2ZjlkMzc1In0.O2SeORSNCBlf8_C3xU9RDZEpshsriXUT4Kx3WdApSQU";
+        Pageable pageable = PageRequest.of(0,3);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Authorization","Bearer "+token);
+
         final String uri_events = "http://testing.gausscontrol.com:8080/processor/events/tasktrigger/upload";
         List<AlertaActivaEntity>alertaActivaEntities = alertaActivaRepository
                 .findAllByFecenvgaussAndFecregBetween(null,horaant,horaact,pageable);
         System.out.println(alertaActivaEntities.size());
+        HttpEntity<List<AlertaActivaEntity>> entity = new HttpEntity<>(alertaActivaEntities,headers);
         //List<Object[]> objects = alertaActivaRepository.getListEventsByMinute(horaant,horaact);
         //System.out.println(objects);
         List<RequestGauss> requestGausses = convertActivaEntity(alertaActivaEntities);
         RestTemplate restTemplate = new RestTemplate();
         //ListRequestGauss listRequestGauss = new ListRequestGauss(requestGausses);
-        ResponseEntity<String> str = restTemplate.postForEntity(uri_events,requestGausses, String.class);
-
+        ResponseEntity<String> str = restTemplate.postForEntity(uri_events,entity, String.class);
+        System.out.println(str);
+        System.out.println(str.getStatusCode());
         Gson gson = new Gson();
         //ListResponseGauss listResponseGauss = gson.fromJson(str.getBody(),ListResponseGauss.class);
         /*if(str.getStatusCodeValue()==200){
